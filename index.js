13810424371bf10d3dee89bbd3cf5350fe78f54d
theo <teddyhoover@gmail.com>
2021-03-18 11:58:45 -0500

    commit tree a71f17c6cf3404ff807470c2a02610b8bec0ac3e

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+    }
+} 
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+    }
+});
+
+function moveDodgerRight() {
+    let rightNum = dodger.style.left.replace("px", "");
+    let right = parseInt(rightNum, 10);
+
+    if (right <1000) {
+    dodger.style.left = `${right + 1}px`;
+    }
+} 
